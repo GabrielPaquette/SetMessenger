92c6493c62edc8bee1c3a05609ce30ecb5ebec4c
jarlaxe227 <jarlaxe227@gmail.com>
2016-11-19 11:15:57 -0500

    Created the Service project, attempting to add gitignore to all subdirs to see if it will work to ignore those pesky debugs

---
+++ ChatSystemService/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace ChatSystemService
+{
+    public static class Logger
+    {
+        private const string eventSourceName = "ChatSource";
+        private const string eventLogName = "SETMessengerLogs";
+
+        public static void Log(string message)
+        {
+            EventLog serviceEventLog = new EventLog();
+            if (!EventLog.SourceExists(eventSourceName))
+            {
+                EventLog.CreateEventSource(eventSourceName, eventLogName);
+            }
+            serviceEventLog.Source = eventSourceName;
+            serviceEventLog.Log = eventLogName;
+            serviceEventLog.WriteEntry(message);
+        }
+    }
+}
